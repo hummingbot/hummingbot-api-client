6c7a4524b3abfc952f32ef423cfaba54ef87c0a3
cardosofede <federico.cardoso.e@gmail.com>
2025-07-03 22:08:56 +0200

    (feat) add sync client for streamlit

--- hummingbot_api_client/__init__.py
+++ hummingbot_api_client/__init__.py
@@ -1,4 +1,5 @@
 from .client import HummingbotAPIClient
+from .sync_client import SyncHummingbotAPIClient
 
 __version__ = "0.1.0"
-__all__ = ["HummingbotAPIClient"]
+__all__ = ["HummingbotAPIClient", "SyncHummingbotAPIClient"]
