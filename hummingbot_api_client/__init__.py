51c64f70fef2e493830209a44a76ffeae2de9b7c
cardosofede <federico.cardoso.e@gmail.com>
2026-04-21 01:00:15 -0300

    (feat) bump version

--- hummingbot_api_client/__init__.py
+++ hummingbot_api_client/__init__.py
@@ -2,5 +2,5 @@ from .client import HummingbotAPIClient
 from .sync_client import SyncHummingbotAPIClient
 from .ws import MarketDataWebSocket, ExecutorsWebSocket, WebSocketRouter
 
-__version__ = "1.4.0"
+__version__ = "1.4.1"
 __all__ = ["HummingbotAPIClient", "SyncHummingbotAPIClient", "MarketDataWebSocket", "ExecutorsWebSocket", "WebSocketRouter"]
